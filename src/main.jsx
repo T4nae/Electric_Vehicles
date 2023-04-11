f2a027d02f7562070bb22a06c507a37460a861be
T4nae <tan.b63000@gmail.com>
2023-04-11 20:34:08 +0530

    blocks mobile users

--- src/main.jsx
+++ src/main.jsx
@@ -4,4 +4,9 @@ import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <>
+    <p id="blocked">Please access this website from Desktop</p>
+        <App className="main" />
+    </>
+);
